87b7328c1a1974c658ba5966cfefbf2fc93ecc0d
Ingafen <ingafen@gmail.com>
2022-01-16 12:55:15 +0500

    Added server ip label to index page

--- GetIpWebApp/Controllers/HomeController.cs
+++ GetIpWebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using Microsoft.AspNetCore.Mvc;
 using GetIpWebApp.Models;
 
@@ -15,6 +18,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["IpAddress"] = GetLocalIpAddress();
         return View();
     }
 
@@ -28,4 +32,47 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    string GetLocalIpAddress()  
+    {  
+        UnicastIPAddressInformation? mostSuitableIp = null;  
+        var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();  
+        foreach (var network in networkInterfaces)  
+        {  
+            if (network.OperationalStatus != OperationalStatus.Up)  
+                continue;  
+      
+            var properties = network.GetIPProperties();  
+            if (properties.GatewayAddresses.Count == 0)  
+                continue;  
+      
+            foreach (var address in properties.UnicastAddresses)  
+            {  
+                if (address.Address.AddressFamily != AddressFamily.InterNetwork)  
+                    continue;  
+      
+                if (IPAddress.IsLoopback(address.Address))  
+                    continue;  
+      
+                if (!address.IsDnsEligible)  
+                {  
+                    if (mostSuitableIp == null)  
+                        mostSuitableIp = address;  
+                    continue;  
+                }  
+      
+                // The best IP is the IP got from DHCP server  
+                if (address.PrefixOrigin != PrefixOrigin.Dhcp)  
+                {  
+                    if (mostSuitableIp is not { IsDnsEligible: true })  
+                        mostSuitableIp = address;  
+                    continue;  
+                }  
+                return address.Address.ToString();  
+            }  
+        }  
+        return mostSuitableIp != null  
+            ? mostSuitableIp.Address.ToString()  
+            : "";  
+    } 
 }
